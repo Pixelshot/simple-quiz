c5999195dc14060b029d362d391a638a4ecac174
Hazri Jordan <hazrijordan@gmail.com>
2018-07-28 11:35:20 +0800

    Completed Event handling, moving on to testing with enzyme

--- src/index.js
+++ src/index.js
@@ -65,8 +65,17 @@ function getTurnData(authors) {
 }
 
 const state = {
-    turnData: getTurnData(authors)
+    turnData: getTurnData(authors),
+    highlight: ''
 };
 
-ReactDOM.render(<App {...state}/>, document.getElementById('root'));
+function onAnswerSelected(answer) {
+  const isCorrect = state.turnData.author.books.some((book) => book === answer);
+  state.highlight = isCorrect ? 'correct' : 'wrong';
+  render();
+}
+function render() {
+  ReactDOM.render(<App {...state} onAnswerSelected={onAnswerSelected}/>, document.getElementById('root'));
+}
+render();
 registerServiceWorker();
